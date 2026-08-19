e92811d9b6c07af19cdaaa76b5947c58df627f7a
Israel Bustamante <israelbustamante3@gmail.com>
2026-08-19 12:29:30 -0600

    Quitar Hyperlink roto (display:none por CSS de brand no cargado) del panel de login

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -1,17 +1,14 @@
-import { getConfig } from '@edx/frontend-platform';
-import { Hyperlink, Image } from '@openedx/paragon';
+import { Image } from '@openedx/paragon';
 import logoOllinBlanco from '../../../assets/Ollin-blanco.png';
 
 const LargeLayout = () => (
   <div className="w-50 d-flex login-brand-panel-inah">
     <div className="col-12 login-brand-panel-inah__bg d-flex align-items-center justify-content-center">
-      <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-        <Image
-          className="login-brand-panel-inah__logo"
-          alt={getConfig().SITE_NAME}
-          src={logoOllinBlanco}
-        />
-      </Hyperlink>
+      <Image
+        className="login-brand-panel-inah__logo"
+        alt="Aula INAH"
+        src={logoOllinBlanco}
+      />
     </div>
   </div>
 );
